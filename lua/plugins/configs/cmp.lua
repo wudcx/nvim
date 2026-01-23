759c42356ddd3596113fc545d5f062c85fc767a5
wudc1 <wudc1@chinafastprint.com>
2026-01-23 13:37:43 +0800

    fix cursorline

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -30,7 +30,7 @@ local options = {
     window = {
         completion = {
             side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
-            winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
+            -- winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
             scrollbar = false,
         },
         documentation = {
